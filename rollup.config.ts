d47b3a57d4445b082da88c9d85efab3a3a7498a2
ModyQyW <wurui-dev@foxmail.com>
2023-01-04 17:50:51 +0800

    chore: update deps

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,11 +1,4 @@
-import { readFileSync } from 'node:fs';
-import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'rollup';
 import { rollupIndexConfig, rollupIndexTypesConfig } from '@modyqyw/utils';
-import type { PackageJson } from 'type-fest';
 
-const packageJson = JSON.parse(
-  readFileSync(fileURLToPath(new URL('./package.json', import.meta.url)), 'utf-8'),
-) as PackageJson;
-
-export default defineConfig([rollupIndexConfig(packageJson), rollupIndexTypesConfig(packageJson)]);
+export default defineConfig([rollupIndexConfig(), rollupIndexTypesConfig()]);
