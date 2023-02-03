1079398bee0fa74d78fa657e650599a1b193f51c
ModyQyW <wurui-dev@foxmail.com>
2023-02-03 22:06:46 +0800

    chore: update example

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,5 +1,10 @@
-import { createSSRApp } from "vue";
-import App from "./App.vue";
+import 'core-js/actual/array/iterator';
+import 'core-js/actual/promise';
+import 'core-js/actual/object/assign';
+import 'core-js/actual/promise/finally';
+import { createSSRApp } from 'vue';
+import App from './App.vue';
+
 export function createApp() {
   const app = createSSRApp(App);
   return {
