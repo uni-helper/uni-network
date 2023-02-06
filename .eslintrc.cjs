90bd9ab12da82141b8684645682b385506243202
ModyQyW <wurui-dev@foxmail.com>
2023-02-06 09:29:37 +0800

    chore: update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,9 @@
 module.exports = {
   extends: [require.resolve('@modyqyw/fabric/eslint')],
+  rules: {
+    'sonarjs/cognitive-complexity': 'off',
+    'unicorn/consistent-destructuring': 'off',
+    'unicorn/no-thenable': 'off',
+    'unicorn/error-message': 'off',
+  },
 };
