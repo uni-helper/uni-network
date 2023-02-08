19139fd91278e78f9676380742f9c08103293167
ModyQyW <wurui-dev@foxmail.com>
2023-02-08 15:11:05 +0800

    build: switch to unbuild

---
+++ build.config.ts
@@ -0,0 +1,22 @@
+import { appendFileSync } from 'node:fs';
+import { resolve } from 'node:path';
+import { defineBuildConfig } from 'unbuild';
+
+export default defineBuildConfig({
+  entries: ['./src/index'],
+  clean: true,
+  declaration: true,
+  rollup: {
+    emitCJS: true,
+    inlineDependencies: true,
+    esbuild: {
+      target: 'es2017',
+    },
+  },
+  hooks: {
+    'build:done': (ctx) => {
+      const cjs = resolve(ctx.options.outDir, 'index.cjs');
+      appendFileSync(cjs, 'module.exports = Object.assign(exports.default || {}, exports);');
+    },
+  },
+});
