c533e9c784330be34307c059e5c6a0a9d5489b30
ModyQyW <wurui-dev@foxmail.com>
2023-07-01 18:48:36 +0800

    chore: update playground

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -2,6 +2,7 @@ import 'core-js/actual/array/iterator';
 import 'core-js/actual/promise';
 import 'core-js/actual/object/assign';
 import 'core-js/actual/promise/finally';
+import 'core-js/actual/url-search-params';
 import { createSSRApp } from 'vue';
 import App from './App.vue';
 
