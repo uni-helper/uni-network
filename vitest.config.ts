2fc9c42884ceabc75e47cc7e3609461c2de2ae12
ModyQyW <wurui-dev@foxmail.com>
2023-11-05 23:42:10 +0800

    chore: update config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,5 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     clearMocks: true,
-    coverage: {
-      provider: 'c8',
-      reporter: ['text', 'json', 'html'],
-    },
   },
 });
