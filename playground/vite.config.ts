3651470811215c296e842d3a93425bf11f4a463e
ModyQyW <wurui-dev@foxmail.com>
2024-02-18 18:24:02 +0800

    chore: update example

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,9 +3,5 @@ import uni from '@dcloudio/vite-plugin-uni';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    target: 'es6',
-    cssTarget: 'chrome61',
-  },
   plugins: [uni()],
 });
