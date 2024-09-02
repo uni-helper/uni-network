7c23594a1fb4c8793cdb452cefaa3b37d9a6ff13
Rui Wu <wurui.dev@gmail.com>
2024-09-02 14:43:03 +0800

    chore: remove extra code

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,10 +4,6 @@ import './custom.css';
 
 const theme: Theme = {
   extends: DefaultTheme,
-  enhanceApp({ app }) {
-    // register your custom global components
-    app.component('MyGlobalComponent' /* ... */);
-  },
 };
 
 export default theme;
